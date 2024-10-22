43d278577969cb4a26af1f81ea03f2c9c8b56ba9
265_Shaula Aljauhara Riyadi <103361453+shaulaaljauharariyadi@users.noreply.github.com>
2024-10-23 00:53:25 +0700

    Add files via upload

---
+++ gopbkk/go.mod
@@ -0,0 +1,3 @@
+module gopbkk
+
+go 1.23.2
---
+++ gopbkk/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var bookingName = "booking kita"
+	const bookingTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("selamat datang di aplikasi", bookingName)
+	fmt.Println("jumlah tiket tersedia", bookingTickets, "tiket dan", remainingTickets, "tersisa.")
+	fmt.Println("dapatkan tiket anda disini")
+
+}
