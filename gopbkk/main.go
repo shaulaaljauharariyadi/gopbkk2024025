c2466ed37a7ac701409726e01ce93927b8ae0cb8
265_Shaula Aljauhara Riyadi <103361453+shaulaaljauharariyadi@users.noreply.github.com>
2024-10-24 02:28:15 +0700

    Update main.go

--- gopbkk/main.go
+++ gopbkk/main.go
@@ -4,12 +4,33 @@ import "fmt"
 
 func main() {
 
-	var bookingName = "booking kita"
-	const bookingTickets = 50
-	var remainingTickets = 50
+	bookingName := "booking kita"
+	const bookingTickets int = 50
+	var remainingTickets uint = 50
 
-	fmt.Println("selamat datang di aplikasi", bookingName)
-	fmt.Println("jumlah tiket tersedia", bookingTickets, "tiket dan", remainingTickets, "tersisa.")
+	fmt.Printf("bookingTickets adalah %T,remainingTickets adalah %T,bookingName adalah %T\n", bookingTickets, remainingTickets, bookingName)
+
+	fmt.Printf("selamat datang di aplikasi %v\n", bookingName)
+	fmt.Printf("jumlah tiket tersedia %v tiket dan %v tersisa.\n", bookingTickets, remainingTickets)
 	fmt.Println("dapatkan tiket anda disini")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTicket int
+	//nama depan user
+	fmt.Println("Masukkan nama depan: ")
+	fmt.Scan(&firstName)
+	//nama belakang user
+	fmt.Println("Masukkan nama belakang: ")
+	fmt.Scan(&lastName)
+	//email user
+	fmt.Println("Masukkan email: ")
+	fmt.Scan(&email)
+	//jumlah tiket yang dibeli user
+	fmt.Println("ingin memesan berapa tiket: ")
+	fmt.Scan(&userTicket)
+
+	fmt.Printf("user %v %v membeli %v tiket,dan kode konfirmasi akan dikirimkan ke email %v\n", firstName, lastName, userTicket, email)
 
 }
