415451d7f0aff4835c9d4bd25d0d6e38193d84cc
265_Shaula Aljauhara Riyadi <103361453+shaulaaljauharariyadi@users.noreply.github.com>
2024-10-31 15:03:28 +0700

    Update main.go

--- gopbkk/main.go
+++ gopbkk/main.go
@@ -5,54 +5,31 @@ import (
 	"strings"
 )
 
+var bookingName = "booking kita"
+
+const bookingTickets int = 50
+
+var remainingTickets uint = 50
+var bookings = []string{}
+
 func main() {
 
-	bookingName := "booking kita"
-	const bookingTickets int = 50
-	var remainingTickets uint = 50
-	bookings := []string{}
-
-	fmt.Printf("selamat datang di aplikasi %v\n", bookingName)
-	fmt.Printf("jumlah tiket tersedia %v tiket dan %v tersisa.\n", bookingTickets, remainingTickets)
-	fmt.Println("dapatkan tiket anda disini")
+	greetUsers()
 
 	for {
 
-		var firstName string
-		var lastName string
-		var email string
-		var userTicket uint
-		//nama depan user
-		fmt.Println("Masukkan nama depan: ")
-		fmt.Scan(&firstName)
-		//nama belakang user
-		fmt.Println("Masukkan nama belakang: ")
-		fmt.Scan(&lastName)
-		//email user
-		fmt.Println("Masukkan email: ")
-		fmt.Scan(&email)
-		//jumlah tiket yang dibeli user
-		fmt.Println("ingin memesan berapa tiket: ")
-		fmt.Scan(&userTicket)
+		// fungsi untuk user input
+		firstName, lastName, email, userTicket := getUserInput()
 
-		isValidName := len(firstName) >= 2 && len(lastName) >= 2
-		isValidEmail := strings.Contains(email, "@")
-		isValidTicketNumber := userTicket > 0 && userTicket <= remainingTickets
+		// fungsi untuk validate input user
+		isValidName, isValidEmail, isValidTicketNumber := validateUserinput(firstName, lastName, email, userTicket)
 
 		if isValidName && isValidEmail && isValidTicketNumber {
-			remainingTickets = remainingTickets - userTicket
 
-			bookings = append(bookings, firstName+" "+lastName)
+			bookTicket(userTicket, firstName, lastName, email)
 
-			fmt.Printf("user %v %v membeli %v tiket,dan kode konfirmasi akan dikirimkan ke email %v\n", firstName, lastName, userTicket, email)
-			fmt.Printf("%v tersisa untuk %v\n", remainingTickets, bookingName)
-
-			firstNames := []string{}
-			for _, booking := range bookings {
-				var names = strings.Fields(booking)
-				firstNames = append(firstNames, names[0])
-			}
-
+			// memanggil fungsi untuk print nama awal
+			firstNames := getFirstNames()
 			fmt.Printf("nama awal dari pembooking adalah: %v\n", firstNames)
 
 			if remainingTickets == 0 {
@@ -73,19 +50,55 @@ func main() {
 		}
 	}
 
-	city := "London"
+}
 
-	switch city {
-	case "New York":
-		//execute code untuk tiket ke new york
-	case "Singapore":
-		//execute code untuk tiket ke singapore
-	case "London", "berlin":
-		//code untuk london & berlin
-	case "Hong Kong":
-		//code
-	default:
-		fmt.Print("tidak ada kota yang dipilih")
+func greetUsers() {
+	fmt.Printf("selamat datang di aplikasi %v\n", bookingName)
+	fmt.Printf("jumlah tiket tersedia %v tiket dan %v tersisa.\n", bookingTickets, remainingTickets)
+	fmt.Println("dapatkan tiket anda disini")
+}
+
+func getFirstNames() []string {
+	firstNames := []string{}
+	for _, booking := range bookings {
+		var names = strings.Fields(booking)
+		firstNames = append(firstNames, names[0])
 	}
+	return firstNames
+}
 
+func validateUserinput(firstName string, lastName string, email string, userTicket uint) (bool, bool, bool) {
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTicketNumber := userTicket > 0 && userTicket <= remainingTickets
+	return isValidName, isValidEmail, isValidTicketNumber
+}
+
+func getUserInput() (string, string, string, uint) {
+	var firstName string
+	var lastName string
+	var email string
+	var userTicket uint
+	//nama depan user
+	fmt.Println("Masukkan nama depan: ")
+	fmt.Scan(&firstName)
+	//nama belakang user
+	fmt.Println("Masukkan nama belakang: ")
+	fmt.Scan(&lastName)
+	//email user
+	fmt.Println("Masukkan email: ")
+	fmt.Scan(&email)
+	//jumlah tiket yang dibeli user
+	fmt.Println("ingin memesan berapa tiket: ")
+	fmt.Scan(&userTicket)
+
+	return firstName, lastName, email, userTicket
+}
+
+func bookTicket(userTicket uint, firstName string, lastName string, email string) {
+	remainingTickets = remainingTickets - userTicket
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("user %v %v membeli %v tiket,dan kode konfirmasi akan dikirimkan ke email %v\n", firstName, lastName, userTicket, email)
+	fmt.Printf("%v tersisa untuk %v\n", remainingTickets, bookingName)
 }
