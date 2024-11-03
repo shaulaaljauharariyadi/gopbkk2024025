8122e9b3c7a758818b1fbbc2b9eb802e75d9fab2
265_Shaula Aljauhara Riyadi <103361453+shaulaaljauharariyadi@users.noreply.github.com>
2024-11-04 02:14:56 +0700

    Update main.go

--- gopbkk/main.go
+++ gopbkk/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"gopbkk/helper"
+	"sync"
+	"time"
 )
 
 var bookingName = "booking kita"
@@ -10,45 +12,55 @@ var bookingName = "booking kita"
 const bookingTickets int = 50
 
 var remainingTickets uint = 50
-var bookings = []string{}
+var bookings = make([]UserData, 0)
+
+type UserData struct {
+	firstName       string
+	lastName        string
+	email           string
+	numberOfTickets uint
+}
+
+var wg = sync.WaitGroup{}
 
 func main() {
 
 	greetUsers()
 
-	for {
+	// fungsi untuk user input
+	firstName, lastName, email, userTicket := getUserInput()
 
-		// fungsi untuk user input
-		firstName, lastName, email, userTicket := getUserInput()
+	// fungsi untuk validate input user
+	isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserinput(firstName, lastName, email, userTicket, remainingTickets)
 
-		// fungsi untuk validate input user
-		isValidName, isValidEmail, isValidTicketNumber := validateUserinput(firstName, lastName, email, userTicket)
+	if isValidName && isValidEmail && isValidTicketNumber {
 
-		if isValidName && isValidEmail && isValidTicketNumber {
+		bookTicket(userTicket, firstName, lastName, email)
 
-			bookTicket(userTicket, firstName, lastName, email)
+		wg.Add(1)
+		go sendTicket(userTicket, firstName, lastName, email)
 
-			// memanggil fungsi untuk print nama awal
-			firstNames := getFirstNames()
-			fmt.Printf("nama awal dari pembooking adalah: %v\n", firstNames)
+		// memanggil fungsi untuk print nama awal
+		firstNames := getFirstNames()
+		fmt.Printf("nama awal dari pembooking adalah: %v\n", firstNames)
 
-			if remainingTickets == 0 {
-				//end program
-				fmt.Println("tiket telah habis terjual,datang lain waktu.")
-				break
-			}
-		} else {
-			if !isValidName {
-				println("nama awal atau akhir yang dimasukkan terlalu pendek!")
-			}
-			if !isValidEmail {
-				println("email yang dimasukkan tidak valid!")
-			}
-			if !isValidTicketNumber {
-				println("jumlah tiket yang dimasukkan tidak valid!")
-			}
+		if remainingTickets == 0 {
+			//end program
+			fmt.Println("tiket telah habis terjual,datang lain waktu.")
+			// break
+		}
+	} else {
+		if !isValidName {
+			println("nama awal atau akhir yang dimasukkan terlalu pendek!")
+		}
+		if !isValidEmail {
+			println("email yang dimasukkan tidak valid!")
+		}
+		if !isValidTicketNumber {
+			println("jumlah tiket yang dimasukkan tidak valid!")
 		}
 	}
+	wg.Wait()
 
 }
 
@@ -61,17 +73,10 @@ func greetUsers() {
 func getFirstNames() []string {
 	firstNames := []string{}
 	for _, booking := range bookings {
-		var names = strings.Fields(booking)
-		firstNames = append(firstNames, names[0])
+
+		firstNames = append(firstNames, booking.firstName)
 	}
 	return firstNames
-}
-
-func validateUserinput(firstName string, lastName string, email string, userTicket uint) (bool, bool, bool) {
-	isValidName := len(firstName) >= 2 && len(lastName) >= 2
-	isValidEmail := strings.Contains(email, "@")
-	isValidTicketNumber := userTicket > 0 && userTicket <= remainingTickets
-	return isValidName, isValidEmail, isValidTicketNumber
 }
 
 func getUserInput() (string, string, string, uint) {
@@ -97,8 +102,27 @@ func getUserInput() (string, string, string, uint) {
 
 func bookTicket(userTicket uint, firstName string, lastName string, email string) {
 	remainingTickets = remainingTickets - userTicket
-	bookings = append(bookings, firstName+" "+lastName)
+
+	// membuat map untuk user
+	var userData = UserData{
+		firstName:       firstName,
+		lastName:        lastName,
+		email:           email,
+		numberOfTickets: userTicket,
+	}
+
+	bookings = append(bookings, userData)
+	fmt.Printf("List Booking %v\n", bookings)
 
 	fmt.Printf("user %v %v membeli %v tiket,dan kode konfirmasi akan dikirimkan ke email %v\n", firstName, lastName, userTicket, email)
 	fmt.Printf("%v tersisa untuk %v\n", remainingTickets, bookingName)
+}
+
+func sendTicket(userTicket uint, firstName string, lastName string, email string) {
+	time.Sleep(25 * time.Second)
+	var ticket = fmt.Sprintf("%v tiket untuk %v %v", userTicket, firstName, lastName)
+	fmt.Println("#####################")
+	fmt.Printf("mengirim tiket:\n %v \n ke email %v\n", ticket, email)
+	fmt.Println("#####################")
+	wg.Done()
 }
